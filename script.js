4ff6084d38e7cab4a98a64e697955198c808d7a4
thegauravverma <gaurav.pune2000@gmail.com>
2024-12-02 21:38:01 -0800

    First Blog

--- script.js
+++ script.js
@@ -39,3 +39,25 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+const slides = document.querySelector('.slides');
+const images = document.querySelectorAll('.slides img');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+let counter = 0;
+const size = images[0].clientWidth;
+
+next.addEventListener('click', () => {
+  if (counter >= images.length -1) return; // Stop at the last image
+  slides.style.transition = 'transform 0.5s ease-in-out';
+  counter++;
+  slides.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+prev.addEventListener('click', () => {
+  if (counter <= 0) return; // Stop at the first image
+  slides.style.transition = 'transform 0.5s ease-in-out';
+  counter--;
+  slides.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
